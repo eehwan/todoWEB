02b5e02b3ab41fae3c20b5c4220c6803d6ffa734
eehwan <eehwan.shin@gmail.com>
2020-07-11 22:11:52 +0900

    fix 'loading location-process'

--- JS/weather.js
+++ JS/weather.js
@@ -1,40 +1,46 @@
-function select(tag){
+function select(tag) {
   return document.querySelector(tag);
 }
 
 const coords = "coords";
 const openWeatherMap_api_Key = "608b9ffa6002fe78ed3a0182ce0b9e75";
-const lang= "uk";
+const lang = "uk";
 
 const _weather = select("#weather"),
-      _place = select("#place");
+  _place = select("#place");
 
-function load_coords(){
-  const loadedCoords = JSON.parse(localStorage.getItem(coords));
-  if (loadedCoords==null){
+function load_coords() {
+  var loadedCoords = JSON.parse(localStorage.getItem(coords));
+  if (loadedCoords == null) {
     ask_coords();
-  }get_weather(loadedCoords.latitude, loadedCoords.longitude);
+  } else {
+    get_weather(loadedCoords);
+  }
 }
+
 function ask_coords(){
   navigator.geolocation.getCurrentPosition(handle_geoSuccess, handle_geoError)
 }
+
 function handle_geoSuccess(location){
-  console.log(location);
-  const locationObj ={
+  const locationObj = {
     latitude: location.coords.latitude,
     longitude: location.coords.longitude
   }
   localStorage.setItem(coords, JSON.stringify(locationObj));
+  get_weather(locationObj);
 }
-function handle_geoError(){
+
+function handle_geoError() {
   alert("Error: can`t load location");
 }
-function get_weather(lat, long){
-  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${openWeatherMap_api_Key}&units=metric&lang=${lang}`
-  ).then(function(response) {
+
+function get_weather(loadedCoords) {
+  const lat = loadedCoords.latitude,
+        long = loadedCoords.longitude;
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${openWeatherMap_api_Key}&units=metric&lang=${lang}`).then(function(response) {
     return response.json()
-  }).then(function(json){
-    // console.log(json);
+  }).then(function(json) {
     _place.innerHTML = `${json.name}`;
     _weather.innerHTML = `Cloud :&nbsp;${json.clouds.all} &nbsp;&nbsp;&nbsp;<br>TEMP : ${json.main.temp.toFixed(1)} &deg;C<br>feels like : ${json.main.feels_like.toFixed(1)} &deg;C`;
   });
@@ -42,7 +48,7 @@ function get_weather(lat, long){
 
 
 
-function init(){
+function init() {
   load_coords();
 }
 init();

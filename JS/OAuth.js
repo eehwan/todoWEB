f130df3f281891d36d6ccf4238f9295baeab335b
eehwan <eehwan.shin@gmail.com>
2020-07-20 16:25:38 +0900

    OAuth...

--- JS/OAuth.js
+++ JS/OAuth.js
@@ -2,6 +2,12 @@ const app_id = '1010943272709060';
 const api_version = 'v7.0';
 const app_secret = '7b35ac8a5d7fc03ac23442c8f3c286d2';
 
+function select(tag){
+  return document.querySelector(tag);
+}
+
+const login_status = select("#status");
+
 window.fbAsyncInit = function() {
   FB.init({
     appId      : `${app_id}`,
@@ -16,17 +22,16 @@ window.fbAsyncInit = function() {
 function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
   console.log('statusChangeCallback');
   console.log(response);                  // The current login status of the person.
-  console.log('-----------------------');
   if (response.status === 'connected') {   // Logged into your webpage and Facebook.
     console.log("is Logined")
-    console.log('-----------------------');
-    
+    login_status.value = 'is Logined'
+
     testAPI();
   } else {                                 // Not logged into your webpage or we are unable to tell.
     console.log("Not Logined")
-    console.log('-----------------------');
 
-    document.getElementById('status').innerHTML = 'Please log into this webpage.';
+    login_status.value = 'Not Logined'
+    login_status.innerHTML = 'Please log into this webpage.';
   }
 }
 
@@ -42,10 +47,27 @@ function testAPI() {                      // Testing Graph API after login.  See
     console.log(`Successful login for: ${response.name}`);
     console.log('-----------------------');
 
-    document.getElementById('status').innerHTML =
+    login_status.innerHTML =
       `Thanks for logging in ${response.name} !`;
   });
 }
 //
 //
 // statusChangeCallback(response);
+
+
+function init(){
+  login_status.addEventListener('click',function(){
+    if (login_status.value == 'is Logined'){
+      FB.login(function(res){
+        console.log("login")
+      })
+    }
+    else{
+      FB.logout(function(res){
+        console.log("logout")
+      })
+    }
+  })
+}
+init()

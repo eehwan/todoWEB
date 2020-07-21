bee3baf6431ae629ce925637c18475273d5d4928
eehwan <eehwan.shin@gmail.com>
2020-07-21 19:34:18 +0900

    OAuth...

--- JS/OAuth.js
+++ JS/OAuth.js
@@ -6,7 +6,10 @@ function select(tag){
   return document.querySelector(tag);
 }
 
-const login_status = select("#status");
+const facebook = select("#fb_button");
+// const fb_login = select('#fb_login');
+// const fb_logout = select('#fb_logout');
+const _text = select('#text');
 
 window.fbAsyncInit = function() {
   FB.init({
@@ -15,57 +18,56 @@ window.fbAsyncInit = function() {
     xfbml      : true,                     // Parse social plugins on this webpage.
     version    : `${api_version}`           // Use this Graph API version for this call.
   });
-  FB.getLoginStatus(function(response) {   // Called after the JS SDK has been initialized.
-    statusChangeCallback(response);        // Returns the login status.
-  });
+  checkLoginState()
 }
 function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
   console.log('statusChangeCallback');
   console.log(response);                  // The current login status of the person.
   if (response.status === 'connected') {   // Logged into your webpage and Facebook.
     console.log("is Logined")
-    login_status.value = 'is Logined'
+    fb_button.value = 'is Logined'
+    fb_button.innerHTML = "LogOut";
 
     testAPI();
   } else {                                 // Not logged into your webpage or we are unable to tell.
     console.log("Not Logined")
+    fb_button.value = 'Not Logined'
+    fb_button.innerHTML = "LogIn";
 
-    login_status.value = 'Not Logined'
-    login_status.innerHTML = 'Please log into this webpage.';
+    _text.innerHTML = 'Please log into this webpage.';
   }
 }
 
-// function checkLoginState() {               // Called when a person is finished with the Login Button.
-//   FB.getLoginStatus(function(response) {   // See the onlogin handler
-//     statusChangeCallback(response);
-//   });
-// }
-//
+function checkLoginState() {               // Called when a person is finished with the Login Button.
+  FB.getLoginStatus(function(response) {   // See the onlogin handler
+    statusChangeCallback(response);
+  });
+}
+
 function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
   console.log('Welcome!  Fetching your information.... ');
   FB.api('/me', function(response) {
     console.log(`Successful login for: ${response.name}`);
     console.log('-----------------------');
 
-    login_status.innerHTML =
+    _text.innerHTML =
       `Thanks for logging in ${response.name} !`;
   });
 }
-//
-//
-// statusChangeCallback(response);
 
 
 function init(){
-  login_status.addEventListener('click',function(){
-    if (login_status.value == 'is Logined'){
-      FB.login(function(res){
-        console.log("login")
+  fb_button.addEventListener('click',function(){
+    if (fb_button.value == 'is Logined'){
+      FB.logout(function(res){
+        console.log("logout");
+        statusChangeCallback(res);
       })
     }
     else{
-      FB.logout(function(res){
-        console.log("logout")
+      FB.login(function(res){
+        console.log("login");
+        statusChangeCallback(res);
       })
     }
   })
